1f2bc7ba4336815b3546153462b19c4ac29217a4
D.A Jalloh <info@codewithjalloh.com>
2016-07-06 00:16:19 +0100

    UI and rotateGesture function added

--- RotateGesture/ViewController.swift
+++ RotateGesture/ViewController.swift
@@ -9,15 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var generalTzu: UIImageView!
+    var netRotation:CGFloat = 0
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let rotateGesture = UIRotationGestureRecognizer(target: self, action: "rotateGesture:")
+        generalTzu.addGestureRecognizer(rotateGesture)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func rotateGesture(sender: UIRotationGestureRecognizer) {
+        
     }
 
 
